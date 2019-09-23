7583830d852f06279ed0570260e4d1c4cae9d1be
Saadat Esmaeily <saadat.esmaeily@gmail.com>
2019-09-24 00:48:32 +0330

    Added follower system model

--- posts/models.py
+++ posts/models.py
@@ -26,6 +26,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio = models.TextField(max_length=1000, blank=True)
+    follows = models.ManyToManyField('Profile', related_name='followed_by', symmetrical=False)
 
     def __str__(self):
         return f'{self.user.username} Profile'
