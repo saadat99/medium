saadat99/medium

commit 4a371c31f9a275c6738adf5a07a2ea8718730979

Follower system early implementation (without ajax)

posts/templates/posts/profile.html
@@ -1,35 +1,45 @@
 {% extends 'posts/layout.html' %}
 {% block content %}
-<div class="col s12 m8 offset-m2 l6 offset-l3">
+<div class="col s10 m8 offset-m2 l6 offset-l3">
   <div class="card-panel grey lighten-5 z-depth-1">
+    
     <div class="row valign-wrapper">
       <div class="col s2">
         <img src="{{ owner.profile.image.url }}" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
       </div>
       <div class="col s10">
-        <h3>{{ owner.username }}</h3>
-        <span class="black-text">
-          {{ owner.profile.bio }}
+        <span>
+          <h3>{{ owner.username }}</h3>
+          {% if not follow_check %}
+          <a href="/accounts/profile/follow/{{ owner.username }}/1/" class="btn waves-effect waves-light grey"><i class="material-icons left">person_add</i>Follow</a>
+          {% else %}
+          <a href="/accounts/profile/follow/{{ owner.username }}/0/" class="btn waves-effect waves-light red"><i class="material-icons left">person_outline</i>Unfollow</a>
+          {% endif %}
         </span>
+        <div class="black-text">
+          {{ owner.profile.bio }}
+        </div>
       </div>
     </div>
+
     <div class="col s12 m6">
       {% for post in posts %}
         <a href="/posts/details/{{post.id}}">
-            <div class="card blue-grey lighten-4">
-                    <div class="card-content black-text">
-                        <span class="card-title">{{ post.title }}</span>
-                        <p>{{ post.body | truncatechars:100 | safe }}</p>
-                        <span class="red-text text-darken-3">{{ post.created_at|date }}</span>
-                    </div>
-                    <!-- <div class="card-action">
-                        <a href="/posts/details/{{post.id}}">Read more</a>
-                    </div> -->
-                </div>  
+          <div class="card blue-grey lighten-4">
+            <div class="card-content black-text">
+                <span class="card-title">{{ post.title }}</span>
+                <p>{{ post.body | truncatechars:100 | safe }}</p>
+                <span class="red-text text-darken-3">{{ post.created_at|date }}</span>
+            </div>
+            <!-- <div class="card-action">
+                <a href="/posts/details/{{post.id}}">Read more</a>
+            </div> -->
+          </div>  
         </a>
-            
         {% endfor %}
     </div>
   </div>
+  <div>hello in dev</div>
 </div>
+<div class="col s2">hello</div>
 {% endblock %}

posts/views.py
@@ -91,12 +91,38 @@ def profile(request, username):
     # posts = Posts.objects.filter(user=request.user).order_by('-created_at')[:10]
     user = get_object_or_404(User, username=username)
     posts = Posts.objects.filter(user=user).order_by('-created_at')[:10]
+
+    # Check for follow button
+    # TODO more efficient way https://docs.djangoproject.com/en/dev/ref/models/querysets/#exists
+    follow_check = False
+    if user.profile in request.user.profile.follows.all():
+        follow_check = True
+    
     context = {
         'title' : 'Latest Posts',
         'posts' : posts,
-        'owner' : user        
+        'owner' : user,
+        'follow_check' : follow_check        
     }
     return render(request, 'posts/profile.html', context)
+
+def follow(request, username, action):
+    user = get_object_or_404(User, username=username)
+    
+    print(action)
+    if action:
+        request.user.profile.follows.add(user.profile)
+        return HttpResponse('added relation')
+    else:
+        request.user.profile.follows.remove(user.profile)
+        return HttpResponse('removed relation')
+
+    # context = {
+    #     'title' : 'Latest Posts',
+    #     'posts' : posts,
+    #     'owner' : user        
+    # }
+    # return render(request, 'posts/profile.html', context)
 
 @login_required
 def settings(request):

posts/templates/posts/includes/navbar.html
@@ -18,7 +18,7 @@
         <!-- <li><a href="/">Home</a></li> -->
         {% if user.is_authenticated %}
             <li>
-                <a class="waves-effect waves-light btn orange lighten-1" href="{% url 'post_new' %}"><i class="material-icons left">add</i>Write new article</a>
+                <a class="waves-effect waves-light btn blue lighten-1" href="{% url 'post_new' %}"><i class="material-icons left">add</i>New article</a>
             </li>
             <!-- Dropdown Trigger -->
             <li>

posts/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
         name='password_change_done'
     ),
     path('accounts/profile/<slug:username>/', views.profile, name='profile'),
+    path('accounts/profile/follow/<slug:username>/<int:action>/', views.follow, name='follow'),
     path('accounts/settings', views.settings, name='settings'),
     path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/login/', auth_views.LoginView.as_view(template_name='myapp/login.html')),

commit d331c7850cece077e7de6e61924fadc15a47f708

Fixed a bug and upgraded requirements 1. Fixed a AnonymousUser bug related to views introduced in 4a371c31f9a275c6738adf5a07a2ea8718730979 2. Updated the profile template 3. Upgraded requirements

posts/templates/posts/profile.html
@@ -10,10 +10,12 @@
       <div class="col s10">
         <span>
           <h3>{{ owner.username }}</h3>
-          {% if not follow_check %}
-          <a href="/accounts/profile/follow/{{ owner.username }}/1/" class="btn waves-effect waves-light grey"><i class="material-icons left">person_add</i>Follow</a>
-          {% else %}
-          <a href="/accounts/profile/follow/{{ owner.username }}/0/" class="btn waves-effect waves-light red"><i class="material-icons left">person_outline</i>Unfollow</a>
+          {% if user.is_authenticated %}
+            {% if not follow_check %}
+            <a href="/accounts/profile/follow/{{ owner.username }}/1/" class="btn waves-effect waves-light grey"><i class="material-icons left">person_add</i>Follow</a>
+            {% else %}
+            <a href="/accounts/profile/follow/{{ owner.username }}/0/" class="btn waves-effect waves-light red"><i class="material-icons left">person_outline</i>Unfollow</a>
+            {% endif %}
           {% endif %}
         </span>
         <div class="black-text">

posts/views.py
@@ -93,10 +93,11 @@ def profile(request, username):
     posts = Posts.objects.filter(user=user).order_by('-created_at')[:10]
 
     # Check for follow button
-    # TODO more efficient way https://docs.djangoproject.com/en/dev/ref/models/querysets/#exists
+    # TODO more efficient way https://docs.djangoproject.com/en/dev/ref/models/querysets/#exists 
     follow_check = False
-    if user.profile in request.user.profile.follows.all():
-        follow_check = True
+    if request.user.is_authenticated:
+        if user.profile in request.user.profile.follows.all():
+            follow_check = True
     
     context = {
         'title' : 'Latest Posts',